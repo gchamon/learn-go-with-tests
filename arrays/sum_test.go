e909fadf2e6149f325330a278f9f311bf2244eee
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2023-10-30 22:35:44 -0300

    change want to illustrate that DeepEqual not type safe

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -21,7 +21,7 @@ func TestSum(t *testing.T) {
 
 func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2, 3}, []int{4, 5, 6})
-	want := []int{6, 15}
+	want := "bob"
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v, want %v", got, want)
