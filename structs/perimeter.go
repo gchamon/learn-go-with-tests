457eaac79c78f94bf0a6bde8631d394c60c0e54f
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2023-11-02 21:24:35 -0300

    add triangle shape

--- structs/perimeter.go
+++ structs/perimeter.go
@@ -23,6 +23,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height / 2
+}
+
 func Perimeter(rec Rectangle) float64 {
 	return 2 * (rec.Height + rec.Width)
 }
