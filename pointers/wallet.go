166d5ee676f6ba096edc172b5485e622ae45c5b8
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2023-11-04 09:43:23 -0300

    refactor error into global variable

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 )
 
+var ErrInsufficientFunds = errors.New("insufficient funds")
+
 type Bitcoin int
 
 func (b Bitcoin) String() string {
@@ -22,7 +24,7 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 func (w *Wallet) Withdraw(amount Bitcoin) error {
 
 	if amount > w.balance {
-		return errors.New("insufficient funds")
+		return ErrInsufficientFunds
 	}
 
 	w.balance -= amount
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,25 +3,6 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	assertError := func(t testing.TB, err error, want string) {
-		t.Helper()
-
-		if err.Error() != want {
-			t.Errorf("got %q, want %q", err, want)
-		}
-
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
-		}
-	}
-
-	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-		if got != want {
-			t.Errorf("got %d, want %s", got, want)
-		}
-	}
 
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
@@ -38,7 +19,27 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{balance: startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 
-		assertError(t, err, "insufficient funds")
+		assertError(t, err, ErrInsufficientFunds)
 		assertBalance(t, wallet, startingBalance)
 	})
+}
+
+func assertError(t testing.TB, err, want error) {
+	t.Helper()
+
+	if err != want {
+		t.Errorf("got %q, want %q", err, want)
+	}
+
+	if err == nil {
+		t.Error("wanted an error but didn't get one")
+	}
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+	if got != want {
+		t.Errorf("got %d, want %s", got, want)
+	}
 }
