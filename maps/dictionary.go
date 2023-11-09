6c893714c27c20da8014ef70b05e2a14fd42f1ac
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2023-11-08 22:47:41 -0300

    implement Search error
    slightly different from reference implementation. Here I wanted to
    exercise the comma ok pattern instead of splitting into two (retrieving
    error and testing)

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,13 @@
 package main
 
+import "errors"
+
 type Dictionary map[string]string
 
 func (d *Dictionary) Search(entry string) (string, error) {
-	return (*d)[entry], nil
+	if definition, ok := (*d)[entry]; ok {
+		return definition, nil
+	} else {
+		return "", errors.New("word unknown")
+	}
 }
