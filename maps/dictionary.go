7d22f6e803709f984f52731b0f117e0a86313f9f
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2023-11-14 21:09:42 -0300

    implement Dictionary.Add
    refactor Search to use copy instead of pointer to dict

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -6,14 +6,14 @@ type Dictionary map[string]string
 
 var ErrNotFound = errors.New("word unknown")
 
-func (d *Dictionary) Search(entry string) (string, error) {
-	if definition, ok := (*d)[entry]; ok {
+func (d Dictionary) Search(entry string) (string, error) {
+	if definition, ok := d[entry]; ok {
 		return definition, nil
 	} else {
 		return "", ErrNotFound
 	}
 }
 
-func (d *Dictionary) Add(entry, definition string) {
-
+func (d Dictionary) Add(entry, definition string) {
+	d[entry] = definition
 }
