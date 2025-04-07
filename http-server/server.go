ea2ac00d09031e285515092193bc7d491b2d9206
Gabriel Chamon <gchamon@tuta.io>
2025-04-07 11:34:45 -0300

    pass method post -> status accepted

--- http-server/server.go
+++ http-server/server.go
@@ -28,13 +28,25 @@ type PlayerServer struct {
 	store PlayerStore
 }
 
-func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
+}
 
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
 	score, err := p.store.GetPlayerScore(player)
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
 	} else {
 		fmt.Fprint(w, score)
+	}
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w)
+	case http.MethodGet:
+		p.showScore(w, r)
 	}
 }
